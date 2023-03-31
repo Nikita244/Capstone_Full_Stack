2569ce51286e7509cb3ca88a7f66d88a5588323b
Nikita244 <giulia.franzosi@hotmail.it>
2023-03-31 16:21:27 +0200

    navbar admin

--- src/app/components/edit-cards/edit-cards.component.ts
+++ src/app/components/edit-cards/edit-cards.component.ts
@@ -34,7 +34,10 @@ export class EditCardsComponent {
     if (this.isLoggedIn) {
       const user = this.storageService.getUser();
       this.roles = user.roles;
+
       this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
       this.username = user.username
 
     }
--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { StorageService } from '../../_services/storage.service';
 import { AuthService } from '../../_services/auth.service';
+import { EventBusService } from '../../_shared/event-bus.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,29 +12,40 @@ export class NavbarComponent implements OnInit {
 
   @Input() isLoggedIn!: boolean;
   @Input() username!: string;
+  @Input() showAdminBoard!: boolean;
+  roles: any;
+  eventBusSub: any;
 
   constructor(private storageService: StorageService,
-    private authService: AuthService) { }
+    private authService: AuthService, private eventBusService: EventBusService) { }
 
-  ngOnInit(): void {
-    const user = this.storageService.getUser();
-    this.username = user.username;
-
-  }
-
-  logout(): void {
-    this.authService.logout().subscribe({
-      next: res => {
-        console.log(res);
-        this.storageService.clean();
-
-        window.location.reload();
-      },
-      error: err => {
-        console.log(err);
+    ngOnInit(): void {
+      this.isLoggedIn = this.storageService.isLoggedIn();
+      if (this.isLoggedIn) {
+        const user = this.storageService.getUser();
+        this.roles = user.roles;
+        this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+        this.username = user.username;
       }
-    });
+
+      this.eventBusSub = this.eventBusService.on('logout', () => {
+        this.logout();
+      });
+    }
+
+    logout(): void {
+      this.authService.logout().subscribe({
+        next: res => {
+          console.log(res);
+          this.storageService.clean();
+          window.location.reload();
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+    }
   }
 
 
-}
+
